4850c39ea383955fda6b0290eda362b4875e61d0
oxygensoset <saveliy.burmistrov.99@mail.ru>
2021-11-02 13:23:49 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int count;
+    for (count = 0; count < 5; count=count + 1 )
+    System.out.println("Переменная цилкла равна " + count);
+    System.out.println("Цикл окончен");
+
+    } // main(String [] ) method
+} // ForStatementDemo calss
